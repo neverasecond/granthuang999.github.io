17eeb8a721397f15d41c059fd081e2366461cca7
GitHub Action <action@github.com>
2026-07-09 09:43:53 +0000

    🚀 chore(incremental): auto build for issue #249

--- docs/plugins/GmeekBSZ.js
+++ docs/plugins/GmeekBSZ.js
@@ -12,5 +12,5 @@ document.addEventListener("DOMContentLoaded", function() {
     var element = document.createElement('script');
     element.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js';
     document.head.appendChild(element);
-    console.log("\n %c GmeekBSZ Plugins https://www.790427.xyz \n","padding:5px 0;background:#bc4c00;color:#fff");
+    console.log("\n %c GmeekBSZ Plugins https://www.futuremedia.work \n","padding:5px 0;background:#bc4c00;color:#fff");
 });
